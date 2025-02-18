9d329b3240f2d6ae52d4faf4f5e880191af946f5
yousof-ali <yousofali293@gmail.com>
2025-02-19 01:04:44 +0600

    new food add

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/',(req,res) => {
-    res.send("Restaurant Management Sarver!!")
+app.get('/', (req, res) => {
+  res.send("Restaurant Management Sarver!!")
 });
 
 
@@ -33,62 +33,90 @@ async function run() {
     const orderCollections = client.db('restaurantDB').collection("order");
 
 
-    app.get('/foods',async(req,res) => {
-        let option = {};
-        if(req.query.category){
-          option = {category:req.query.category}
-        }
-        const result = await foodCollections.find(option).toArray();
-        res.send(result);
+    app.get('/foods', async (req, res) => {
+      let query = {}
+      if(req.query.category && req.query.category !== 'all'){
+        query.category = req.query.category
+      }
+      const page = parseInt(req.query.page) || 0;
+      const size = parseInt(req.query.size) || 10;
+      const result = await foodCollections.find(query).skip(page * size).limit(size).toArray();
+      res.send(result);
+      console.log(query)
     });
     
-    app.get('/single-food/:id',async(req,res) => {
+
+    app.post('/new-food',async(req,res) => {
+      const data = req.body
+      const result = await foodCollections.insertOne(data);
+      res.send(result);
+    })
+
+
+    app.get('/search-food',async(req,res) => {
+      const query = req.query.search
+      const option = {name:{$regex:query,$options:'i'}};
+      const result = await foodCollections.find(option).toArray();
+      res.send(result);
+    })
+
+    app.get('/single-food/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await foodCollections.findOne(query);
-      console.log(result);
       res.send(result)
     });
 
-    app.put('/quantity/:id',async(req,res) => {
+    app.get('/count', async (req, res) => {
+      const query = req.query.category
+      let option = {}
+      if(query !== 'all'){
+        option = {category:query}
+      }
+      const result = await foodCollections.countDocuments(option)
+      res.send({ count: result });
+     
+    })
+
+    app.put('/quantity/:id', async (req, res) => {
       const id = req.params.id
       const data = req.body.quantity
       const result = await foodCollections.updateOne(
-        {_id:new ObjectId(id)},
-        {$set:{quantity:data}}
+        { _id: new ObjectId(id) },
+        { $set: { quantity: data } }
       )
       res.send(result);
     })
 
-    app.post('/order',async(req,res) => {
+    app.post('/order', async (req, res) => {
       const body = req.body
       const result = await orderCollections.insertOne(body);
       res.send(result);
     })
 
-    app.get('/top-purchase',async(req,res) => {
+    app.get('/top-purchase', async (req, res) => {
       const purchaseData = await orderCollections.aggregate([
         {
-          $group: {_id:"$product_ID",totalPurchase:{$sum:1}}
+          $group: { _id: "$product_ID", totalPurchase: { $sum: 1 } }
         },
         {
-          $sort:{totalPurchase:-1}
+          $sort: { totalPurchase: -1 }
         },
         {
-          $limit:6
+          $limit: 6
         }
       ]).toArray();
       const productID = purchaseData.map(item => new ObjectId(item._id));
-      const products = await foodCollections.find({_id: {$in:productID}}).toArray();
+      const products = await foodCollections.find({ _id: { $in: productID } }).toArray();
 
       const mergeData = purchaseData.map(purchaseItem => {
         const product = products.find(prod => prod._id.toString() === purchaseItem._id.toString())
-        return  product? {...product,totalPurchase:purchaseItem.totalPurchase} :null;
+        return product ? { ...product, totalPurchase: purchaseItem.totalPurchase } : null;
       }).filter(item => item !== null);
       res.send(mergeData);
 
     })
-   
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -102,6 +130,6 @@ run().catch(console.dir);
 
 
 
-app.listen(port,() => {
-    console.log(`server is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`)
 })
