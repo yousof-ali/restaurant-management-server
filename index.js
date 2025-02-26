651b5fb739726b44871a73dc6e8304e0db2aae8e
yousof-ali <yousofali293@gmail.com>
2025-02-27 00:59:01 +0600

    deploy vercel

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 
 app.use(cors({
   origin:[
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://restaurant-management-39848.web.app',
+    'https://restaurant-management-39848.firebaseapp.com'
   ],
   credentials:true
 }));
@@ -53,7 +55,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const foodCollections = client.db('restaurantDB').collection("foods");
     const orderCollections = client.db('restaurantDB').collection("order");
     const favoriteCollections = client.db('restaurantDB').collection('favorite')
@@ -257,7 +259,7 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
